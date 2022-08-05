a42121f1a55d4ca57753b6dd1bee1e22cad063a8
tore-statsig <74584483+tore-statsig@users.noreply.github.com>
2022-08-05 09:29:56 -0600

    Replace inets with hackney (#3)
    * Replace inets with hackney
    
    * Fix network logevnet
    
    * remove key

--- test/consistency_test.erl
+++ test/consistency_test.erl
@@ -6,13 +6,15 @@ gate_test() ->
   ApiKey = os:getenv("test_api_key"),
   application:set_env(statsig, statsig_api_key, ApiKey),
   application:start(statsig),
-
-  Body = network:request(ApiKey, "rulesets_e2e_test", #{}, true),
-  TestData =  maps:get(<<"data">>, jiffy:decode(Body, [return_maps]), []),
-  lists:map(fun (Data) -> test_input(Data) end, TestData),
-  statsig:log_event(#{<<"userID">> => <<"321">>}, <<"custom_event">>, 12, #{<<"test">> => <<"val">>}),
+  {ok, _Apps} = application:ensure_all_started(statsig),
+  
+  Body = network:request(ApiKey, "rulesets_e2e_test", #{}),
+  statsig:log_event(#{<<"userID">> => <<"321">>}, <<"newevent">>, 12, #{<<"test">> => <<"val">>}),
   statsig:log_event(#{<<"userID">> => <<"456">>}, <<"custom_event">>, <<"hello">>, #{<<"123">> => <<"444">>}),
   statsig:log_event(#{<<"userID">> => <<"12345">>}, <<"custom_event">>, #{<<"test">> => <<"val">>}),
+  statsig:flush(),
+  TestData =  maps:get(<<"data">>, jiffy:decode(Body, [return_maps]), []),
+  lists:map(fun (Data) -> test_input(Data) end, TestData),
   statsig:flush().
 
 test_input(Input) ->
