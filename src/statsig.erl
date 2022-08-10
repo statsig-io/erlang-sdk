c46ef9ad3445fe701b32d76cba30e44dafaf3156
tore-statsig <74584483+tore-statsig@users.noreply.github.com>
2022-08-10 10:14:50 -0700

    Add support for environment tier (#9)
    * Add support for environment tier
    
    * only set env if unset

--- src/statsig.erl
+++ src/statsig.erl
@@ -18,23 +18,29 @@
 start(_Type, _Args) -> statsig_sup:start_link().
 
 -spec check_gate(map(), binary()) -> boolean().
-check_gate(User, Gate) -> gen_server:call(statsig_server, {gate, User, Gate}).
+check_gate(User, Gate) -> 
+  NormalizedUser = utils:get_user_with_environment(User),
+  gen_server:call(statsig_server, {gate, NormalizedUser, Gate}).
 
 -spec get_config(map(), binary()) -> map().
 get_config(User, Config) ->
-  gen_server:call(statsig_server, {config, User, Config}).
+  NormalizedUser = utils:get_user_with_environment(User),
+  gen_server:call(statsig_server, {config, NormalizedUser, Config}).
 
 -spec get_experiment(map(), binary()) -> map().
 get_experiment(User, Experiment) ->
-  gen_server:call(statsig_server, {config, User, Experiment}).
+  NormalizedUser = utils:get_user_with_environment(User),
+  gen_server:call(statsig_server, {config, NormalizedUser, Experiment}).
 
 -spec log_event(map(), binary(), map()) -> none().
 log_event(User, EventName, Metadata) ->
-  gen_server:cast(statsig_server, {log, User, EventName, undefined, Metadata}).
+  NormalizedUser = utils:get_user_with_environment(User),
+  gen_server:cast(statsig_server, {log, NormalizedUser, EventName, undefined, Metadata}).
 
 -spec log_event(map(), binary(), binary() | number(), map()) -> none().
 log_event(User, EventName, Value, Metadata) ->
-  gen_server:cast(statsig_server, {log, User, EventName, Value, Metadata}).
+  NormalizedUser = utils:get_user_with_environment(User),
+  gen_server:cast(statsig_server, {log, NormalizedUser, EventName, Value, Metadata}).
 
 -spec flush() -> none().
 flush() -> gen_server:cast(statsig_server, flush).
--- src/utils.erl
+++ src/utils.erl
@@ -1,7 +1,7 @@
 -module(utils).
 
 -export(
-  [get_timestamp/0, get_sdk_type/0, get_sdk_version/0, get_statsig_metadata/0]
+  [get_timestamp/0, get_sdk_type/0, get_sdk_version/0, get_statsig_metadata/0, get_user_with_environment/1]
 ).
 
 -spec get_timestamp() -> list().
@@ -19,3 +19,22 @@ get_sdk_version() -> "0.1.0".
 
 -spec get_sdk_type() -> list().
 get_sdk_type() -> "erlang-server".
+
+
+-spec get_user_with_environment(map()) -> map().
+get_user_with_environment(User) ->
+  case maps:is_key(<<"statsigEnvironment">>, User) of
+    true ->
+      User;
+    false ->
+      set_env(User)
+  end.
+
+set_env(User) ->
+  case application:get_env(statsig, statsig_environment_tier) of   
+    undefined ->
+      User;
+    {ok, Tier} ->
+      StatsigEnvironment = #{<<"tier">> => Tier},
+      maps:put(<<"statsigEnvironment">>, StatsigEnvironment, User)
+  end.
