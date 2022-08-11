8783b54d262dcda1e88514fa9f2d527196b4f4b7
tore-statsig <74584483+tore-statsig@users.noreply.github.com>
2022-08-10 18:22:32 -0700

    update test

--- test/logging_test.erl
+++ test/logging_test.erl
@@ -7,4 +7,10 @@ private_attribute_test() ->
   Event = logging:get_event(PrivateUser, <<"test_event">>, 42, #{}),
   EventUser = maps:get(<<"user">>, Event, undefined),
   
-  ?assert(maps:get(<<"privateAttributes">>, EventUser, undefined) == undefined).
+  ?assert(maps:get(<<"privateAttributes">>, EventUser, undefined) == undefined),
+
+  PublicUser = #{<<"userID">> => <<"jkw">>, <<"email">> => <<"hello@statsig.com">>},
+  Event2 = logging:get_event(PublicUser, <<"test_event">>, 42, #{}),
+  EventUser2 = maps:get(<<"user">>, Event2, undefined),
+  ?assert(maps:get(<<"privateAttributes">>, EventUser2, undefined) == undefined),
+  ?assert(maps:get(<<"email">>, EventUser2, <<"">>) == <<"hello@statsig.com">>).
